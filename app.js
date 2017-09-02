91961e308f15f6a16e86acfaa29651dfdae9778e
40371231H <40371231H@live.ntnu.edu.tw>
2017-09-02 13:17:32 +0800

    weather 初步查詢

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv-extended').load();
+
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -16,7 +18,56 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+var DialogLabels = {
+    Weathers: 'Weathers',
+    Times: 'Times'
+};
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s !!", session.message.text);
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        // prompt for search option
+        builder.Prompts.choice(
+            session,
+            'Are you looking for a flight or a hotel?',
+            [DialogLabels.Times, DialogLabels.Weathers],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Not a valid option'
+            });
+    },
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Ooops! Too many attemps :( But don\'t worry, I\'m handling that exception and you can try again!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Failed with message: %s', err.message);
+            session.endDialog();
+        });
+
+        // continue on proper dialog
+        var selection = result.response.entity;
+        switch (selection) {
+            case DialogLabels.Times:
+                return session.beginDialog('times');
+            case DialogLabels.Weathers:
+                return session.beginDialog('weathers');
+        }
+    }
+]);
+
+bot.dialog('times', require('./times'));
+bot.dialog('weathers', require('./weathers'));
+bot.dialog('support', require('./support'))
+    .triggerAction({
+        matches: [/help/i, /support/i, /problem/i]
+    });
+
+// log any bot errors into the console
+bot.on('error', function (e) {
+    console.log('And error ocurred', e);
 });
